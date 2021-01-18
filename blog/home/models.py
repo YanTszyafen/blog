95ef53da7bf80615561a718d49bb75d16d8a7666
YanTszyafen <41192330+YanTszyafen@users.noreply.github.com>
2021-01-18 03:14:52 +0300

    'article_model'

--- blog/home/models.py
+++ blog/home/models.py
@@ -16,3 +16,30 @@ class ArticleCategory(models.Model):
         db_table = 'tb_category' #modify table name
         verbose_name = 'Category management' #admin site display
         verbose_name_plural = verbose_name
+
+
+from users.models import User
+from django.utils import timezone
+class Article(models.Model):
+    #on_delete: When the data in the user table is deleted, the article information is also deleted synchronously
+    author = models.ForeignKey(User,on_delete=models.CASCADE)
+    avatar = models.ImageField(upload_to='article/%Y%m%d/',blank=True)
+    title = models.CharField(max_length=50,blank=True)
+    category = models.ForeignKey(ArticleCategory,null=True,blank=True,on_delete=models.CASCADE,related_name='article')
+    tags = models.CharField(max_length=50,blank=True)
+    summary = models.CharField(max_length=600,null=False,blank=False)
+    content = models.TextField()
+    total_views = models.PositiveIntegerField(default=0)
+    comments_count = models.PositiveIntegerField(default=0)
+    created = models.DateTimeField(default=timezone.now)
+    updated = models.DateTimeField(auto_now=True)
+
+    #Modify the table name and configuration information.
+    class Meta:
+        db_table = 'tb_article'
+        ordering = ('-created',)
+        verbose_name = 'Article management'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.title
