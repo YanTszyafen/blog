8e1fca71d113ff465e8d735184e93f6720f51aa4
YanTszyafen <41192330+YanTszyafen@users.noreply.github.com>
2021-01-19 22:54:38 +0300

    'post_comment_and_paging'

--- blog/home/thefilmblog.py
+++ blog/home/thefilmblog.py
@@ -1,18 +1,25 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://thefilm.blog/"
+URL = "https://thefilm.blog/"
+url_news = "https://thefilm.blog/category/news/"
+url_reviews = "https://thefilm.blog/category/reviews/"
+url_previews = "https://thefilm.blog/category/previews/"
 
-def get_article():
-    html = requests.get(url).text
-    soup = BeautifulSoup(html, "html.parser")
+
+def get_article(url):
+    url_ = url
     article = []
-    for id, articles in enumerate(soup.find_all("article")):
-        category = articles.find("span").get_text()
-        title  = articles.find("h1").get_text()
-        img = articles.find("img").get("src")
-        detail_url = articles.find("h1").find("a").get("href")
-        article.append({"category": category,"title": title,"avatar":img, "url": detail_url})
+    for p in range(1,5):
+        url = url_ + "page/"+str(p)+"/"
+        html = requests.get(url).text
+        soup = BeautifulSoup(html, "html.parser")
+        for id, articles in enumerate(soup.find_all("article")):
+            category = articles.find("span", {"class": "cat-links"}).get_text()
+            title = articles.find("h1").get_text()
+            img = articles.find("img").get("src")
+            detail_url = articles.find("h1").find("a").get("href")
+            article.append({"category": category, "title": title, "avatar": img, "url": detail_url})
     return article
 
 def get_detail(detail_url):
@@ -29,3 +36,4 @@ def get_detail(detail_url):
         print(p)
     detail.append({"category":category,"title": title,"created":created,"author":author,"content":content})
     return detail
+
--- blog/home/views.py
+++ blog/home/views.py
@@ -1,39 +1,31 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from django.urls import reverse
 from django.views import View
 from home.models import ArticleCategory, Article
 from django.http.response import HttpResponseNotFound
+from django.core.paginator import Paginator, EmptyPage
 # Create your views here.
 class IndexView(View):
     def get(self,request):
-        """
-        1. Receive info of category
-        2. Get the category id clicked by user
-        3. Query category by the category id
-        4. Get paging parameters
-        5. Query article data by the info of paging
-        6. Create Paginator
-        7. Paging
-        8. Translate data to templates
-        :param request:
-        :return:
-        """
         #1. Receive info of category
         categories = ArticleCategory.objects.all()
         # 2. Get the category id clicked by user
         cat_id = request.GET.get('cat_id',1)
+        # 4. Get paging parameters
+        page_num = request.GET.get('page_num', 1)
+        page_size = request.GET.get('page_size', 6)
         # 3. Query category by the category id
         try:
             category = ArticleCategory.objects.get(id=cat_id)
         except ArticleCategory.DoesNotExist:
             return HttpResponseNotFound('No such category!')
-        #4. Get paging parameters
-        page_num = request.GET.get('page_num',1)
-        page_size = request.GET.get('page_size',6)
+
         # 5. Query article data by the info of paging
         articles = Article.objects.filter(category=category)
         # 6. Create Paginator
-        from django.core.paginator import Paginator, EmptyPage
-        paginator = Paginator(articles,per_page=page_size)
+
+        paginator = Paginator(articles,page_size)
         # 7. Paging
         try:
             page_articles = paginator.page(page_num)
@@ -54,18 +46,18 @@ class IndexView(View):
         return render(request,'index.html',context=context)
 
 
+from home.models import Comment
 class DetailView(View):
     def get(self,request):
-        """
-        1. Receive article id
-        2. Query article data by id
-        3. Query category data
-        4. Translate data to templates
-        :param request:
-        :return:
-        """
         #1. Receive article id
         id = request.GET.get('id')
+        # 4. Get paging parameters
+        page_size = request.GET.get('page_size', 6)
+        print(page_size)
+        page_num = request.GET.get('page_num', 1)
+        print(page_num)
+        # 3. Query category data
+        categories = ArticleCategory.objects.all()
         # 2. Query article data by id
         try:
             article = Article.objects.get(id=id)
@@ -75,17 +67,65 @@ class DetailView(View):
             article.total_views += 1
             article.save()
 
-        # 3. Query category data
-        categories = ArticleCategory.objects.all()
-
         # Query the top 10 article data
         hot_articles = Article.objects.order_by('-total_views')[:9]
 
-        # 4. Translate data to templates
+        # 5. Query comment data by the info of paging
+        comments = Comment.objects.filter(article=article).order_by('-created')
+
+        total_count = comments.count()
+        # 6. Create Paginator
+        paginator = Paginator(comments,page_size)
+        # 7. Paging
+        try:
+            page_comments = paginator.page(page_num)
+        except EmptyPage:
+            return HttpResponseNotFound('Empty Page!')
+        print(paginator.page(1))
+        # print(paginator.page(2))
+        # print(paginator.page(3))
+        total_page = paginator.num_pages
+
+        # 8. Translate data to templates
         context = {
             'categories':categories,
             'category':article.category,
             'article':article,
-            'hot_articles':hot_articles
+            'hot_articles':hot_articles,
+            'total_count':total_count,
+            'comments':page_comments,
+            'page_size':page_size,
+            'total_page':total_page,
+            'page_num':page_num
         }
         return render(request,'detail.html',context=context)
+
+    def post(self,request):
+        #1. Receive user info
+        user = request.user
+        # 2. Check user is logged in or not
+        if user and user.is_authenticated:
+            # 3. Logged in users can post form data
+            #     3.1 Receive comment data
+            id = request.POST.get('id')
+            content = request.POST.get('content')
+            #     3.2 Check the article is exist or not
+            try:
+                article = Article.objects.get(id=id)
+            except Article.DoesNotExist:
+                return HttpResponseNotFound('No such article!')
+            #     3.3 Save comment data
+            Comment.objects.create(
+                content=content,
+                article=article,
+                user=user
+            )
+            #     3.4 Modify the number of comments on the article
+            article.comments_count += 1
+            article.save()
+
+            path = reverse('home:detail')+'?id={}'.format(article.id)
+            return redirect(path)
+        else:
+            # 4. Not logged in users will jump to the login page
+            return redirect(reverse('users:login'))
